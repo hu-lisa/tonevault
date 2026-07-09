5f1aaa08cfe8ea580cd198ad60fffca7bc42cd6d
hu-lisa <lisahu.kaboom@gmail.com>
2026-07-08 21:09:58 -0400

    Sign out button

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -1,16 +1,21 @@
+'use client'
+import { logOut } from "@/app/actions/login";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 
 
 export default function Page() {
     return (
-        <div className="flex flex-col">
-            <header>Profile</header>
-            <span>Email: xyz@gmail.com</span>
+        <div className="flex flex-col space-y-2">
+            <header className="text-2xl">Profile</header>
+            <div className="flex flex-row justify-between items-center">
+                <p>Email: xyz@gmail.com</p>
+                <Button variant="outline" onClick={() => logOut()}>Log Out</Button>
+            </div>
             <Separator />
-            <div className="flex flex-row">
-                <Button variant="outline">Change Password</Button>
+            <div className="flex flex-row-reverse space-x-2">
                 <Button variant="destructive">Delete Account</Button>
+                <Button variant="outline">Change Password</Button>
             </div>
         </div>
     )
